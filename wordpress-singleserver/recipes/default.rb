aa88afa56f19e12a84936365b228c6fafb59ed88
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-15 23:11:54 -0500

    agregamos recetas iniciales

--- wordpress-singleserver/recipes/default.rb
+++ wordpress-singleserver/recipes/default.rb
@@ -1,5 +1,20 @@
-#
-# Cookbook:: wordpress-singleserver
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+
+package "git" do
+  # workaround for:
+  # WARNING: The following packages cannot be authenticated!
+  # liberror-perl
+  # STDERR: E: There are problems and -y was used without --force-yes
+  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+end
+
+application app_path do
+  environment.update("PORT" => "80")
+  environment.update(app["environment"])
+
+  git app_path do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+  end
+end
