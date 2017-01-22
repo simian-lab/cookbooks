fa10e18623d87ce9c1bb2b199714527f7c7fe9ae
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-22 17:45:55 -0500

    added webapp config for wordpress recipe

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -1,12 +1,23 @@
+include_recipe 'apt::default'
+
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
 application app_path do
-  environment.update(app["environment"])
+  environment.update(app['environment'])
 
   git app_path do
-    repository app["app_source"]["url"]
-    revision app["app_source"]["revision"]
-    deploy_key app["app_source"]["ssh_key"]
+    repository app['app_source']['url']
+    revision app['app_source']['revision']
+    deploy_key app['app_source']['ssh_key']
+  end
+
+  web_app app['shortname'] do
+    template 'default-site.conf.erb'
+    server_name app['domains'].first
+    server_aliases app['domains'].drop(1)
+    docroot app_path
   end
 end
+
+include_recipe 'apache2::mod_php5'
