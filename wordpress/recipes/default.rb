41e58e4af2df0da6ae234140efc9b00c0ab4074d
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-27 00:00:04 -0500

    we change vars

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -21,11 +21,6 @@ application app_path do
   end
 end
 
-# We make sure PHP can read ENV vars
-node.set['php']['directives'] = {
-  'variables_order' => 'EGPCS'
-}
-
 # Installing some required packages
 include_recipe 'php::default'
 include_recipe 'php::module_mysql'
@@ -49,4 +44,12 @@ bash "update_env_vars" do
   code <<-EOS
   source /etc/environment
   EOS
+end
+
+# Now we need to make sure php.ini can read the variables
+ruby_block "insert_env_vars" do
+  block do
+    file = Chef::Util::FileEdit.new('/etc/php/7.0/apache2/php.ini')
+    file.search_file_replace_line /^variables_order =/, "variables_order = \"GPCS\""
+  end
 end
