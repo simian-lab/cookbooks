e7397c7516a298509b0b7d74079d3e1317331d68
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-28 16:34:14 -0500

    mix up the order

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -1,26 +1,12 @@
 # Installing some required packages
 include_recipe 'apt::default'
+include_recipe 'apache2::default'
+include_recipe 'php::default'
+include_recipe 'php::module_mysql'
+include_recipe 'apache2::mod_php'
 
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
-
-application app_path do
-  environment.update(app['environment'])
-
-  git app_path do
-    repository app['app_source']['url']
-    revision app['app_source']['revision']
-    deploy_key app['app_source']['ssh_key']
-  end
-
-  # We create the site
-  web_app app['shortname'] do
-    template 'web_app.conf.erb'
-    server_name app['domains'].first
-    server_aliases app['domains'].drop(1)
-    docroot app_path
-  end
-end
 
 # map the environment_variables node to ENV variables
 ruby_block "insert_env_vars" do
@@ -42,14 +28,28 @@ bash "update_env_vars" do
   EOS
 end
 
-include_recipe 'php::default'
-include_recipe 'php::module_mysql'
-include_recipe 'apache2::mod_php'
-
 # Now we need to make sure php.ini can read the variables
-ruby_block "insert_env_vars" do
-  block do
-    file = Chef::Util::FileEdit.new('/etc/php/7.0/apache2/php.ini')
-    file.search_file_replace_line /^variables_order =/, "variables_order = \"GPCS\""
+# ruby_block "insert_env_vars" do
+#   block do
+#     file = Chef::Util::FileEdit.new('/etc/php/7.0/apache2/php.ini')
+#     file.search_file_replace_line /^variables_order =/, "variables_order = \"GPCS\""
+#   end
+# end
+
+application app_path do
+  environment.update(app['environment'])
+
+  git app_path do
+    repository app['app_source']['url']
+    revision app['app_source']['revision']
+    deploy_key app['app_source']['ssh_key']
+  end
+
+  # We create the site
+  web_app app['shortname'] do
+    template 'web_app.conf.erb'
+    server_name app['domains'].first
+    server_aliases app['domains'].drop(1)
+    docroot app_path
   end
 end
