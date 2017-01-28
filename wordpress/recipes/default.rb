abd4a6827b43ba15a705278fc149b2309be28a82
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-28 18:44:26 -0500

    a little mistake with envvars fixed

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -13,7 +13,7 @@ ruby_block "insert_env_vars" do
     file = Chef::Util::FileEdit.new('/etc/apache2/envvars')
     app['environment'].each do |key, value|
       Chef::Log.info("Setting ENV variable #{key}= #{key}=\"#{value}\"")
-      file.insert_line_if_no_match /^#{key}\=/, "#{key}=\"#{value}\""
+      file.insert_line_if_no_match /^export #{key}\=/, "export #{key}=\"#{value}\""
       file.write_file
     end
   end
