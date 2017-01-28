60af5e38485c75971c9cebdf25f7854c069568a3
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-28 18:23:01 -0500

    changing the envvars task, it should work now

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -1,6 +1,5 @@
 # Installing some required packages
 include_recipe 'apt::default'
-# include_recipe 'apache2::default'
 include_recipe 'php::default'
 include_recipe 'php::module_mysql'
 include_recipe 'apache2::mod_php'
@@ -8,10 +7,10 @@ include_recipe 'apache2::mod_php'
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
-# map the environment_variables node to ENV variables
+# Set the environment variables for PHP
 ruby_block "insert_env_vars" do
   block do
-    file = Chef::Util::FileEdit.new('/etc/environment')
+    file = Chef::Util::FileEdit.new('/etc/apache2/envvars')
     app['environment'].each do |key, value|
       Chef::Log.info("Setting ENV variable #{key}= #{key}=\"#{value}\"")
       file.insert_line_if_no_match /^#{key}\=/, "#{key}=\"#{value}\""
@@ -19,22 +18,6 @@ ruby_block "insert_env_vars" do
     end
   end
 end
-
-# We make sure the ENV vars are usable from the start
-bash "update_env_vars" do
-  user "root"
-  code <<-EOS
-  source /etc/environment
-  EOS
-end
-
-# Now we need to make sure php.ini can read the variables
-# ruby_block "insert_env_vars" do
-#   block do
-#     file = Chef::Util::FileEdit.new('/etc/php/7.0/apache2/php.ini')
-#     file.search_file_replace_line /^variables_order =/, "variables_order = \"GPCS\""
-#   end
-# end
 
 application app_path do
   environment.update(app['environment'])
