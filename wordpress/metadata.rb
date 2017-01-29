f0999444444e82535eeab59bcba190621dcbb9d0
Ivan Vasquez <ivancamilov@gmail.com>
2017-01-29 18:29:40 -0500

    added a varnish recipe. Let's hope it works!

--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -11,6 +11,7 @@ depends 'application_git', '~> 1.1.0'
 depends 'apt', '~> 5.0.1'
 depends 'apache2', '~> 3.2.2'
 depends 'php', '~> 2.2.0'
+depends 'varnish', '~> 3.0.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
