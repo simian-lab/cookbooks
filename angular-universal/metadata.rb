8091dfe994dd213cdc075e7b2072e876b362f5c8
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-06 03:14:15 -0500

    updated the angular cookbooks

--- angular-universal/metadata.rb
+++ angular-universal/metadata.rb
@@ -8,15 +8,5 @@ version '0.1.0'
 
 depends 'application', '~> 5.0'
 depends 'application_git', '~> 1.1.0'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/angular-universal/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/angular-universal' if respond_to?(:source_url)
+depends 'apt', '~> 5.0.1'
+depends 'chef_nginx', '~> 5.0.6'
---
+++ angular-universal/recipes/deploy.rb
@@ -0,0 +1,12 @@
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+
+application app_path do
+  environment.update(app["environment"])
+
+  git app_path do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+    deploy_key app["app_source"]["ssh_key"]
+  end
+end
