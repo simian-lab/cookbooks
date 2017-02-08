cd88d2a816727908f22d51f6a6ff3ef2da26ea63
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-08 14:31:24 -0500

    trying out the defaults

--- angular-universal/recipes/default.rb
+++ angular-universal/recipes/default.rb
@@ -1,11 +1,9 @@
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
-node_version = 'v6.2.2'
-
 include_recipe 'apt::default'
 include_recipe 'chef_nginx::default'
-include_recipe "nodejs::nodejs_from_source"
+include_recipe "nodejs::default"
 
 nodejs_npm "angular-cli" do
   json true
