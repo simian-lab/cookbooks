576f09d4e127cb87c03072dfab4d95750ec56d1d
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-08 09:31:41 -0500

    we add the ng build block

--- angular-universal/recipes/default.rb
+++ angular-universal/recipes/default.rb
@@ -3,3 +3,15 @@ app_path = "/srv/#{app['shortname']}"
 
 include_recipe 'apt::default'
 include_recipe 'chef_nginx::default'
+include_recipe 'nvm::default'
+
+nvm_install 'v6.2.2' do
+  from_source false
+  alias_as_default true
+  action :create
+end
+
+execute 'build_ng' do
+  command "ng build --prod"
+  cwd app_path
+end
