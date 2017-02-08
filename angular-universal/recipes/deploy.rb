0fe8fc6d9be4b80225c2f1445f8a37a12815a7c0
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-08 17:54:26 -0500

    making sure we deploy the proper app

--- angular-universal/recipes/deploy.rb
+++ angular-universal/recipes/deploy.rb
@@ -1,16 +1,12 @@
-search("aws_opsworks_app").each do |app|
-  if app['deploy']
-    app = search(:aws_opsworks_app).first
-    app_path = "/srv/#{app['shortname']}"
+app = search("aws_opsworks_app","deploy:true").first
+app_path = "/srv/#{app['shortname']}"
 
-    application app_path do
-      environment.update(app["environment"])
+application app_path do
+  environment.update(app["environment"])
 
-      git app_path do
-        repository app["app_source"]["url"]
-        revision app["app_source"]["revision"]
-        deploy_key app["app_source"]["ssh_key"]
-      end
-    end
+  git app_path do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+    deploy_key app["app_source"]["ssh_key"]
   end
 end
