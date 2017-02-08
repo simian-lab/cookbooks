e6158eaff4688864eadf10df04900155cc94c4eb
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-08 11:59:36 -0500

    we make sure npm is available

--- angular-universal/recipes/default.rb
+++ angular-universal/recipes/default.rb
@@ -1,19 +1,24 @@
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
+node_version = 'v6.2.2'
+
 include_recipe 'apt::default'
 include_recipe 'chef_nginx::default'
 include_recipe 'nvm::default'
 
-nvm_install 'v6.2.2' do
+nvm_install node_version do
   from_source false
   alias_as_default true
   action :create
 end
 
+nvm_alias_default node_version do
+  action :create
+end
+
 execute 'install_angular' do
   command "npm install -g angular-cli"
-  cwd app_path
 end
 
 execute 'install_dependencies' do
