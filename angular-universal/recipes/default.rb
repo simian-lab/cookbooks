7eaea38017f51ce85d2360314b352327e20f9d16
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-08 16:27:52 -0500

    temp change

--- angular-universal/recipes/default.rb
+++ angular-universal/recipes/default.rb
@@ -5,10 +5,9 @@ node_version = 'v6.2.2'
 include_recipe 'apt::default'
 include_recipe 'chef_nginx::default'
 include_recipe 'nvm'
-
-nvm_install node_version do
-  user 'nginx'
-  from_source false
-  alias_as_default true
-  action :create
-end
+#
+# nvm_install node_version do
+#   from_source false
+#   alias_as_default true
+#   action :create
+# end
