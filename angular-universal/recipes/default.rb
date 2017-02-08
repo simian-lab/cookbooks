18658cc0b654939af6c449b4318aa5219426c5ff
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-08 14:44:29 -0500

    make sure npm works

--- angular-universal/recipes/default.rb
+++ angular-universal/recipes/default.rb
@@ -4,8 +4,8 @@ app_path = "/srv/#{app['shortname']}"
 include_recipe 'apt::default'
 include_recipe 'chef_nginx::default'
 include_recipe "nodejs::default"
+include_recipe "nodejs::npm"
 
 nodejs_npm "angular-cli" do
-  json true
   options ['-g']
 end
