2e37854ce78c457efff28d7a35972fa2db00b155
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-15 15:25:34 -0500

    removed user execute

--- angular-universal/recipes/deploy.rb
+++ angular-universal/recipes/deploy.rb
@@ -16,13 +16,11 @@ end
 execute 'install_dependencies' do
   command "npm install"
   cwd app_path
-  user 'nginx'
 end
 
 execute 'build_ng' do
   command "ng build --prod"
   cwd app_path
-  user 'nginx'
 end
 
 # make sure permissions are correct
