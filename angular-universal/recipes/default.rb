b2f923d406a58581f96ceda3c3bbd2e1dd00a3a0
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-16 16:41:10 -0500

    replicating all app search changes to the other recipes

--- angular-universal/recipes/default.rb
+++ angular-universal/recipes/default.rb
@@ -1,39 +1,45 @@
-app = search(:aws_opsworks_app).first
-app_path = "/srv/#{app['shortname']}"
-node_version = 'v6.2.2'
+instance = search("aws_opsworks_instance").first
+layer = instance['layer_ids'].first
 
-include_recipe 'apt::default'
-include_recipe 'chef_nginx::default'
-include_recipe 'chef_nginx::http_v2_module'
+search("aws_opsworks_app","deploy:true").each do |candidate_app|
+  if candidate_app['environment']['LAYER_ID'] == layer
+    app = candidate_app
+    app_path = "/srv/#{app['shortname']}"
 
-template '/etc/nginx/sites-enabled/000-default' do
-  source 'nginx.erb'
-  variables({
-    server_name: app['domains'].first,
-    docroot: "#{app_path}/dist"
-  })
-end
+    include_recipe 'apt::default'
+    include_recipe 'chef_nginx::default'
+    include_recipe 'chef_nginx::http_v2_module'
 
-execute "add_node_dep" do
-  command "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -"
-  user "root"
-  action :run
-end
+    template '/etc/nginx/sites-enabled/000-default' do
+      source 'nginx.erb'
+      variables({
+        server_name: app['domains'].first,
+        docroot: "#{app_path}/dist"
+      })
+    end
 
-execute "install_node" do
-  command "sudo apt-get install -y nodejs"
-  user "root"
-  action :run
-end
+    execute "add_node_dep" do
+      command "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -"
+      user "root"
+      action :run
+    end
 
-execute "install_angular" do
-  command "sudo npm install angular-cli -g"
-  user "root"
-  action :run
-end
+    execute "install_node" do
+      command "sudo apt-get install -y nodejs"
+      user "root"
+      action :run
+    end
 
-execute "restart_nginx" do
-  command "service nginx restart"
-  user "root"
-  action :run
+    execute "install_angular" do
+      command "sudo npm install angular-cli -g"
+      user "root"
+      action :run
+    end
+
+    execute "restart_nginx" do
+      command "service nginx restart"
+      user "root"
+      action :run
+    end
+  end
 end
--- angular-universal/recipes/deploy.rb
+++ angular-universal/recipes/deploy.rb
@@ -1,11 +1,7 @@
 instance = search("aws_opsworks_instance").first
 layer = instance['layer_ids'].first
 
-app = false
-app_path = false
-
 search("aws_opsworks_app","deploy:true").each do |candidate_app|
-  Chef::Log.info("Comparing #{candidate_app['environment']['LAYER_ID']} to #{layer}")
   if candidate_app['environment']['LAYER_ID'] == layer
     app = candidate_app
     app_path = "/srv/#{app['shortname']}"
