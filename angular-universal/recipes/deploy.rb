64241146f927180eac3d9baf6d5ab30eab8bd8fb
Ivan Vasquez <ivancamilov@gmail.com>
2017-02-16 15:58:48 -0500

    we initialise some vars

--- angular-universal/recipes/deploy.rb
+++ angular-universal/recipes/deploy.rb
@@ -1,6 +1,9 @@
 instance = search("aws_opsworks_instance").first
 layer = instance['layer_ids'].first
 
+app = false
+app_path = false
+
 search("aws_opsworks_app","deploy:true").each do |candidate_app|
   if candidate_app['environment']['LAYER_ID'] == layer
     app = candidate_app
@@ -11,36 +14,36 @@ end
 if !app
   app = search("aws_opsworks_app", "deploy:true").first
   app_path = "/srv/#{app['shortname']}"
-end
 
-Chef::Log.info("Deploying #{app['shortname']}")
+  Chef::Log.info("Deploying #{app['shortname']}")
 
-application app_path do
-  environment.update(app["environment"])
+  application app_path do
+    environment.update(app["environment"])
 
-  git app_path do
-    repository app["app_source"]["url"]
-    revision app["app_source"]["revision"]
-    deploy_key app["app_source"]["ssh_key"]
+    git app_path do
+      repository app["app_source"]["url"]
+      revision app["app_source"]["revision"]
+      deploy_key app["app_source"]["ssh_key"]
+    end
   end
-end
 
-# make sure permissions are correct
-execute "chown-data-www" do
-  command "chown -R www-data:www-data #{app_path}"
-  user "root"
-  action :run
-  not_if "stat -c %U #{app_path} | grep www-data"
-end
+  # make sure permissions are correct
+  execute "chown-data-www" do
+    command "chown -R www-data:www-data #{app_path}"
+    user "root"
+    action :run
+    not_if "stat -c %U #{app_path} | grep www-data"
+  end
 
-execute 'install_dependencies' do
-  user "root"
-  command "npm install"
-  cwd app_path
-end
+  execute 'install_dependencies' do
+    user "root"
+    command "npm install"
+    cwd app_path
+  end
 
-execute 'build_ng' do
-  user "root"
-  command "ng build --prod"
-  cwd app_path
+  execute 'build_ng' do
+    user "root"
+    command "ng build --prod"
+    cwd app_path
+  end
 end
