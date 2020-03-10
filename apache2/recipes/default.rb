9e22a3feccaa4f3e97aa751830d6697c0a728d78
Zheiran <juansebastiang2@gmail.com>
2020-03-10 14:40:35 -0500

    Try restart without definition

--- apache2/recipes/default.rb
+++ apache2/recipes/default.rb
@@ -218,7 +218,6 @@ service 'apache2' do
   when 'arch'
     service_name apache_service_name
   end
-  supports [:start, :restart, :reload, :status]
-  action [:enable, :reload]
+  action [:enable, :restart]
   only_if "#{node['apache']['binary']} -t", :environment => { 'APACHE_LOG_DIR' => node['apache']['log_dir'] }, :timeout => 10
 end
