3549fe2d06cac615a1cd7910469e29cd94bb9593
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2023-09-29 11:41:02 -0500

    Add the "noresvport" parameter for the EFS mount.

--- wordpress/recipes/multiserver.rb
+++ wordpress/recipes/multiserver.rb
@@ -60,19 +60,19 @@ end
 # 3. We mount the folders as EFS folders
 if app['environment']['EFS_UPLOADS']
   execute 'mount_uploads' do
-    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_UPLOADS']}:/ #{app_path}/wp-content/uploads"
+    command "sudo mount -t nfs4 -o noresvport,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_UPLOADS']}:/ #{app_path}/wp-content/uploads"
   end
 end
 
 if app['environment']['EFS_GALLERY']
   execute 'mount_gallery' do
-    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_GALLERY']}:/ #{app_path}/wp-content/gallery"
+    command "sudo mount -t nfs4 -o noresvport,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_GALLERY']}:/ #{app_path}/wp-content/gallery"
   end
 end
 
 if app['environment']['EFS_AUTHORS']
   execute 'mount_authors' do
-    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_AUTHORS']}:/ #{app_path}/wp-content/authors"
+    command "sudo mount -t nfs4 -o noresvport,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_AUTHORS']}:/ #{app_path}/wp-content/authors"
   end
 end
 
