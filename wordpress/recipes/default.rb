b885bf0df09df03b89a9a570670f7f99e6c40252
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2024-04-29 13:19:00 -0500

    feat(default.rb): remove the test code

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -37,17 +37,7 @@
 
 
 # Initial setup: just a couple vars we need
-#app = search(:aws_opsworks_app).first
-#app = data_bag('EnvironmentVariables')
-#require 'aws-sdk-ssm'
-
-ssm = Aws::SSM::Client.new(region: 'us-west-2')
-
-resp = ssm.get_parameter({
-  name: 'arn:aws:ssm:us-west-2:969355835785:parameter/ApplyChefRecipes-Preset/Externado-Dev-WordPress-4eddee/EnvironmentVariables',
-  with_decryption: false
-})
-
+app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
 # Installing some required packages
