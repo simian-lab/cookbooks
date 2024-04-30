e443b71e611754745d6cf2f0a7564c9a8a549360
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2024-04-30 15:55:10 -0500

    feat(deploy.rb): comment some code

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,33 +1,33 @@
-app = search("aws_opsworks_app","deploy:true").first
-app_path = "/srv/#{app['shortname']}"
+# app = search("aws_opsworks_app","deploy:true").first
+# app_path = "/srv/#{app['shortname']}"
 
 log 'Current recipe' do
   message 'Running the deploy recipe for WordPress.'
   level :info
 end
 
-execute 'Add an exception for this directory' do
-  command "git config --global --add safe.directory #{app_path}"
-  user "root"
-end
+# execute 'Add an exception for this directory' do
+#   command "git config --global --add safe.directory #{app_path}"
+#   user "root"
+# end
 
-application app_path do
-  environment.update(app['environment'])
+# application app_path do
+#   environment.update(app['environment'])
 
-  git app_path do
-    repository app['app_source']['url']
-    revision app['app_source']['revision']
-    deploy_key app['app_source']['ssh_key']
-  end
-end
+#   git app_path do
+#     repository app['app_source']['url']
+#     revision app['app_source']['revision']
+#     deploy_key app['app_source']['ssh_key']
+#   end
+# end
 
 # make sure permissions are correct
-execute "chown-data-www" do
-  command "chown -R www-data:www-data #{app_path}"
-  user "root"
-  action :run
-  not_if "stat -c %U #{app_path} | grep www-data"
-end
+# execute "chown-data-www" do
+#   command "chown -R www-data:www-data #{app_path}"
+#   user "root"
+#   action :run
+#   not_if "stat -c %U #{app_path} | grep www-data"
+# end
 
 # Clean cache
 service 'varnish' do
@@ -37,14 +37,14 @@ end
 # and now, W3TC's cloudfront config
 cloudfront_config = ""
 
-if app['environment']['CLOUDFRONT_DISTRIBUTION']
-  cloudfront_config = app['environment']['CLOUDFRONT_DISTRIBUTION']
+# if app['environment']['CLOUDFRONT_DISTRIBUTION']
+#   cloudfront_config = app['environment']['CLOUDFRONT_DISTRIBUTION']
 
-  ruby_block 'cloudfront_edit' do
-    block do
-      w3config = Chef::Util::FileEdit.new("#{app_path}/wp-content/w3tc-config/master.php")
-      w3config.search_file_replace_line("cdn.cf2.id", "\"cdn.cf2.id\": \"#{cloudfront_config}\",")
-      w3config.write_file
-    end
-  end
-end
+#   ruby_block 'cloudfront_edit' do
+#     block do
+#       w3config = Chef::Util::FileEdit.new("#{app_path}/wp-content/w3tc-config/master.php")
+#       w3config.search_file_replace_line("cdn.cf2.id", "\"cdn.cf2.id\": \"#{cloudfront_config}\",")
+#       w3config.write_file
+#     end
+#   end
+# end
