2bde20b23a86522c64d9b72b5b329af12aa935be
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2024-05-06 17:25:31 -0500

    feat(deploy.rb): uncomment some code

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,33 +1,65 @@
-# app = search("aws_opsworks_app","deploy:true").first
-# app_path = "/srv/#{app['shortname']}"
+aws_ssm_parameter_store 'getAppSourceUrl' do
+  path '/ApplyChefRecipes-Preset/Externado-Dev-WordPress-4eddee/APP_SOURCE_URL'
+  return_key 'app_source_url'
+  action :get
+end
+
+aws_ssm_parameter_store 'getAppSourceRevision' do
+  path '/ApplyChefRecipes-Preset/Externado-Dev-WordPress-4eddee/APP_SOURCE_REVISION'
+  return_key 'app_source_revision'
+  action :get
+end
+
+aws_ssm_parameter_store 'getAppSourceSsh_Key' do
+  path '/ApplyChefRecipes-Preset/Externado-Dev-WordPress-4eddee/APP_SOURCE_SSH_KEY'
+  return_key 'app_source_ssh_key'
+  action :get
+end
+
+app = {
+  'app_source' => {
+    'url' => node.run_state['app_source_url']
+    'revision' => node.run_state['app_source_revision'],
+    'ssh_key' => node.run_state['app_source_ssh_key']
+  }
+}
 
 log 'Current recipe' do
   message 'Running the deploy recipe for WordPress.'
   level :info
 end
 
-# execute 'Add an exception for this directory' do
-#   command "git config --global --add safe.directory #{app_path}"
-#   user "root"
-# end
+aws_ssm_parameter_store 'getShortName' do
+  path '/ApplyChefRecipes-Preset/Externado-Dev-WordPress-4eddee/SHORT_NAME'
+  return_key 'short_name'
+  action :get
+end
 
-# application app_path do
-#   environment.update(app['environment'])
+app['short_name'] = node.run_state['short_name'];
+app_path = "/srv/#{app['short_name']}"
 
-#   git app_path do
-#     repository app['app_source']['url']
-#     revision app['app_source']['revision']
-#     deploy_key app['app_source']['ssh_key']
-#   end
-# end
+execute 'Add an exception for this directory' do
+  command "git config --global --add safe.directory #{app_path}"
+  user "root"
+end
 
-# make sure permissions are correct
-# execute "chown-data-www" do
-#   command "chown -R www-data:www-data #{app_path}"
-#   user "root"
-#   action :run
-#   not_if "stat -c %U #{app_path} | grep www-data"
-# end
+application app_path do
+  environment.update(app['environment'])
+
+  git app_path do
+    repository app['app_source']['url']
+    revision app['app_source']['revision']
+    deploy_key app['app_source']['ssh_key']
+  end
+end
+
+#make sure permissions are correct
+execute "chown-data-www" do
+  command "chown -R www-data:www-data #{app_path}"
+  user "root"
+  action :run
+  not_if "stat -c %U #{app_path} | grep www-data"
+end
 
 # Clean cache
 service 'varnish' do
@@ -37,14 +69,14 @@ end
 # and now, W3TC's cloudfront config
 cloudfront_config = ""
 
-# if app['environment']['CLOUDFRONT_DISTRIBUTION']
-#   cloudfront_config = app['environment']['CLOUDFRONT_DISTRIBUTION']
+if app['environment']['CLOUDFRONT_DISTRIBUTION']
+  cloudfront_config = app['environment']['CLOUDFRONT_DISTRIBUTION']
 
-#   ruby_block 'cloudfront_edit' do
-#     block do
-#       w3config = Chef::Util::FileEdit.new("#{app_path}/wp-content/w3tc-config/master.php")
-#       w3config.search_file_replace_line("cdn.cf2.id", "\"cdn.cf2.id\": \"#{cloudfront_config}\",")
-#       w3config.write_file
-#     end
-#   end
-# end
+  ruby_block 'cloudfront_edit' do
+    block do
+      w3config = Chef::Util::FileEdit.new("#{app_path}/wp-content/w3tc-config/master.php")
+      w3config.search_file_replace_line("cdn.cf2.id", "\"cdn.cf2.id\": \"#{cloudfront_config}\",")
+      w3config.write_file
+    end
+  end
+end
