a3b0c7d087e27d5765feb76e5aa060cca5ef3400
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2024-05-07 17:48:13 -0500

    feat(deploy.rb): add the lazy method to the git command

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -57,9 +57,9 @@ application app_path do
   environment.update(app['environment'])
 
   git app_path do
-    repository app['app_source']['url']
-    revision app['app_source']['revision']
-    deploy_key app['app_source']['ssh_key']
+    repository lazy {app['app_source']['url']}
+    revision lazy {app['app_source']['revision']}
+    deploy_key lazy {app['app_source']['ssh_key']}
   end
 end
 
