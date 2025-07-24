95a7159d2d6d83b9ab996d354a10b589e9f68908
Nicolás Saiz <nicolasaiz173@gmail.com>
2025-07-24 16:35:30 -0500

    ci: add some missing "noresvport" from mount commands

--- wordpress/recipes/singleserver.rb
+++ wordpress/recipes/singleserver.rb
@@ -19,12 +19,12 @@ end
 
 if app['environment']['EFS_GALLERY']
   execute 'mount_gallery' do
-    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_GALLERY']}:/ #{app_path}/wp-content/gallery"
+    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport #{app['environment']['EFS_GALLERY']}:/ #{app_path}/wp-content/gallery"
   end
 end
 
 if app['environment']['EFS_AUTHORS']
   execute 'mount_authors' do
-    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 #{app['environment']['EFS_AUTHORS']}:/ #{app_path}/wp-content/authors"
+    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport #{app['environment']['EFS_AUTHORS']}:/ #{app_path}/wp-content/authors"
   end
 end
